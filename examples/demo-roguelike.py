01ca4e6860da132c8521cdff9cde62d6c74aafc6
Kiyoshi Aman <kiyoshi.aman@gmail.com>
2012-12-14 07:09:22 -0500

    examples/demo-roguelike: Optimize output some more.

--- examples/demo-roguelike.py
+++ examples/demo-roguelike.py
@@ -5,10 +5,14 @@ class Cell:
     glyph = '.'
 
     def __init__(self):
+        self.player = False
         self.items = []
 
     def __str__(self):
-        return self.glyph
+        if self.player:
+            return '@'
+        else:
+            return self.glyph
 
 class Talon:
     def __init__(self, name, tin, tout):
@@ -27,6 +31,7 @@ class Talon:
         ]
         self.player_x = int(self.cols / 2)
         self.player_y = int(self.rows / 2)
+        self.grid[self.player_y][self.player_x].player = True
 
         self.term.on_key = self.input()
         self.term.clear()
@@ -45,8 +50,6 @@ class Talon:
             row = ''.join([str(x) for x in self.grid[y]])
             self.term.print(bytes(row, 'UTF-8'))
 
-        self.term.goto(self.player_y, self.player_x)
-        self.term.print('@')
         self.term.goto(self.rows + 1, 0)
         self.term.print('Name: %s' % (self.name))
 
@@ -55,14 +58,15 @@ class Talon:
             self.msg = ' ' * self.cols if not self.msg.startswith(' ') else ''
 
             key = event.str.decode('UTF-8')
+            self.grid[self.player_y][self.player_x].player = False
 
             if key == 'Up':
-                if self.player_y == 1:
+                if self.player_y == 0:
                     self.msg = 'My, what a marvelous bedrock wall.'
                 else:
                     self.player_y -= 1
             elif key == 'Down':
-                if self.player_y == self.rows:
+                if self.player_y == self.rows - 1:
                     self.msg = 'My, what a marvelous bedrock wall.'
                 else:
                     self.player_y += 1
@@ -76,6 +80,8 @@ class Talon:
                     self.msg = 'My, what a marvelous bedrock wall.'
                 else:
                     self.player_x += 1
+
+            self.grid[self.player_y][self.player_x].player = True
         return handler
 
 if __name__ == '__main__':
