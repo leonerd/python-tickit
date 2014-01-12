eb6710c1329d215e6a458e8df022b9949c5f1971
Kiyoshi Aman <kiyoshi.aman@gmail.com>
2014-01-12 06:39:50 -0600

    gen_tickit: ctypeslib changed their filenames. :|

--- gen_tickit.sh
+++ gen_tickit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo 'Exporting tickit.h to XML...'
-h2xml -o _tickit.xml /usr/include/tickit.h 2> /dev/null
+h2xml.py -o _tickit.xml /usr/include/tickit.h 2> /dev/null
 echo 'Importing XML into tickit._tickit...'
-xml2py -o tickit/_tickit.py _tickit.xml -r Tickit.* -r TICKIT_.* -r tickit_.* 2> /dev/null
+xml2py.py -o tickit/_tickit.py _tickit.xml -r Tickit.* -r TICKIT_.* -r tickit_.* 2> /dev/null
 echo
