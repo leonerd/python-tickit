4984d20a0ffc1ba2ee0432a1104a253f3e7e2814
Kiyoshi Aman <kiyoshi.aman@gmail.com>
2014-08-13 18:37:25 -0500

    And remove the hardcoded path.

--- gen_tickit.sh
+++ gen_tickit.sh
@@ -3,5 +3,5 @@
 echo 'Exporting tickit.h to XML...'
 h2xml -o _tickit.xml /usr/include/tickit.h 2> /dev/null
 echo 'Importing XML into tickit._tickit...'
-xml2py -o tickit/_tickit.py _tickit.xml -r Tickit.* -r TICKIT_.* -r tickit_.* -l /usr/lib/libtickit.so 2> /dev/null
+xml2py -o tickit/_tickit.py _tickit.xml -r Tickit.* -r TICKIT_.* -r tickit_.* -l libtickit.so 2> /dev/null
 echo
