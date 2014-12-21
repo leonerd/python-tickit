b58b11ee973ce2c5920eb402013fece38673561c
Kiyoshi Aman <kiyoshi.aman@gmail.com>
2014-12-21 13:01:01 -0600

    Remove autogeneration, since we're now planning on manual maintenance.

--- gen_tickit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo 'Exporting tickit.h to XML...'
-h2xml -o _tickit.xml /usr/include/tickit.h 2> /dev/null
-echo 'Importing XML into tickit._tickit...'
-xml2py -o tickit/_tickit.py _tickit.xml -r Tickit.* -r TICKIT_.* -r tickit_.* -l libtickit.so 2> /dev/null
-echo
